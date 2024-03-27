be3462622942192cbcbfc0005719776c6f57f164
unknown <wadah.faisal99@gmail.com>
2024-03-27 16:59:34 +0200

    hero initial setup

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
-import heroBcg from "../assets/hero-bcg.jpeg";
-import heroBcg2 from "../assets/hero-bcg-2.jpeg";
+// import heroBcg from "../assets/hero-bcg.jpeg";
+// import heroBcg2 from "../assets/hero-bcg-2.jpeg";
+import heroBcg from "../assets/product.jpg";
 
 const Hero = () => {
   return (
     <section className="home-hero">
+      <article className="img-container">
+        <img src={heroBcg} alt="nice table" className="main-img" />
+        {/* <img src={heroBcg2} alt="person working" className="accent-img" /> */}
+      </article>
       <article className="content">
         <h1>
           design your
@@ -18,10 +23,6 @@ const Hero = () => {
         <Link to="/products" className="hero-btn">
           shop now
         </Link>
-      </article>
-      <article className="img-container">
-        <img src={heroBcg} alt="nice table" className="main-img" />
-        <img src={heroBcg2} alt="person working" className="accent-img" />
       </article>
     </section>
   );
